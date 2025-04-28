50575223d5acc98ec89bd30d61201ce4874e0a41
Niarfe <efrain.olivares@gmail.com>
2025-04-28 01:05:38 -0700

    Add package files setup and init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name="montecarlo",
+        version='0.1',
+        description='DS5100 Final Project',
+        url='http://github.com/Niarfe/DS5100_project',
+        author='Efrain Olivares',
+        licence='MIT',
+        packages=['montecarlo'],
+        install_requires=['pandas', 'numpy']
+)
